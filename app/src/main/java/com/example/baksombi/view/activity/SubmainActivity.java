327ea71aefb4599729ce5b8ac0dec0d95b79a08f
Rakotonaivo Rado Tiako Sombiniaina <sombitiako@gmail.com>
2022-05-18 22:17:17 +0300

    preferences interface integration

---
+++ app/src/main/java/com/example/baksombi/view/activity/SubmainActivity.java
@@ -0,0 +1,16 @@
+package com.example.baksombi.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.baksombi.R;
+
+public class SubmainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submain);
+    }
+}
