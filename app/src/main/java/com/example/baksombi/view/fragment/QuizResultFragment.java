735a5408edf72d297d05851a0f53b511a53cd90e
Rakotonaivo Rado Tiako Sombiniaina <sombitiako@gmail.com>
2022-05-19 23:34:26 +0300

    Quiz new checkout

--- app/src/main/java/com/example/baksombi/view/fragment/QuizResultFragment.java
+++ app/src/main/java/com/example/baksombi/view/fragment/QuizResultFragment.java
@@ -1,4 +1,4 @@
-package com.example.baksombi;
+package com.example.baksombi.view.fragment;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.baksombi.R;
 
 /**
  * A simple {@link Fragment} subclass.
